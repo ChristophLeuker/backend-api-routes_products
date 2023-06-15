588326bffbd8b1dbc3a54ff6621004b19f030262
Christoph Leuker <cleuker87@gmail.com>
2023-06-15 16:19:52 +0200

    solution

---
+++ pages/products/index.js
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+
+export default function ProductList() {
+  const { data, isLoading } = useSWR("/api/products");
+  console.log(data);
+
+  if (isLoading) {
+    return <h1>Loading ...</h1>;
+  }
+  if (!data) {
+    return <h1> cant load data</h1>;
+  }
+
+  return (
+    <>
+      <ul>
+        {data.map((dates) => (
+          <li key={dates.id}>
+            id: {dates.id}, name: {dates.name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
